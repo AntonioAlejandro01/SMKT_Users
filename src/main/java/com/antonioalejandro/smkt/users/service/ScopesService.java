45e187929f2198182b8ed41a06fb002a109d593c
AntonioAlejandro01 <antonioalejandroserranoramirez@gmail.com>
2020-12-04 00:03:38 +0100

    update scope service

--- src/main/java/com/antonioalejandro/smkt/users/service/ScopesService.java
+++ src/main/java/com/antonioalejandro/smkt/users/service/ScopesService.java
@@ -2,11 +2,20 @@ package com.antonioalejandro.smkt.users.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ScopesService implements IScopesService {
 
+	@Autowired
+	private IRoleService roleService;
+	
 	@Override
 	public List<String> getScopesForRole(Long roleId) {
-		return null;
+		
+		List<Scopes> scopes = roleService.getRoleById(roleId).get
 	}
 
 }
