969555e933360418824049447ae5084e3b0ad8fd
AntonioAlejandro01 <antonioalejandroserranoramirez@gmail.com>
2020-12-06 22:14:35 +0100

    add covergae

--- src/test/java/com/antonioalejandro/smkt/users/model/test/entity/test/UserTest.java
+++ src/test/java/com/antonioalejandro/smkt/users/model/test/entity/test/UserTest.java
@@ -1,10 +1,12 @@
 package com.antonioalejandro.smkt.users.model.test.entity.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 import com.antonioalejandro.smkt.users.UtilsForTesting;
+import com.antonioalejandro.smkt.users.entity.Role;
 import com.antonioalejandro.smkt.users.entity.User;
 
 class UserTest {
@@ -15,10 +17,24 @@ class UserTest {
 		User user1 = UtilsForTesting.getUser();
 
 		assertEquals(UtilsForTesting.DATAOK, user1.getRoleName());
+		assertEquals(UtilsForTesting.DATAOK, user1.getEmail());
+		assertEquals(UtilsForTesting.DATAOK, user1.getLastname());
+		assertEquals(UtilsForTesting.DATAOK, user1.getName());
+		assertEquals(UtilsForTesting.DATAOK, user1.getPassword());
+		assertEquals(UtilsForTesting.DATAOK, user1.getUsername());
+		assertEquals(UtilsForTesting.LONGOK, user1.getId());
+		assertThat(user1.getRole()).isInstanceOf(Role.class);
 
 		UtilsForTesting.modifyUser(user1);
 
 		assertEquals(UtilsForTesting.DATAKO, user1.getRoleName());
+		assertEquals(UtilsForTesting.DATAKO, user1.getRoleName());
+		assertEquals(UtilsForTesting.DATAKO, user1.getEmail());
+		assertEquals(UtilsForTesting.DATAKO, user1.getLastname());
+		assertEquals(UtilsForTesting.DATAKO, user1.getName());
+		assertEquals(UtilsForTesting.DATAKO, user1.getPassword());
+		assertEquals(UtilsForTesting.DATAKO, user1.getUsername());
+		assertEquals(UtilsForTesting.LONGOK, user1.getId());
 
 	}
 }
